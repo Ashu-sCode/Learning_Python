19f54d3bc51509a085abed6398da81d241fa701e
Ashu-sCode <att567893393@gmail.com>
2025-12-11 17:30:11 +0530

    function

--- basic.py
+++ basic.py
@@ -2,6 +2,14 @@
 # variable delcaration
 randomVar=1
 
+# printing hello world
+print("Hello, World!")
+
+# taking input 
 n=int(input("Enter a number: "))
+
+# for loop to print multiplication table
 for i in range (1,6):
     print(i,"*",n, "=", i*n)
+
+# function to add two number 
